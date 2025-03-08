2dd87c66e1dd860b557a7d679f9534834fa7a8f8
Elgta <mvdploeg1992@icloud.com>
2025-03-08 02:51:04 +0100

    Update Bools.cs

--- AimmyWPF/Class/Bools.cs
+++ AimmyWPF/Class/Bools.cs
@@ -1,21 +1,21 @@
-﻿namespace AimmyWPF.Class
+namespace AimmyWPF.Class
 {
     internal class Bools
     {
-        public static bool AIAimAligner = false;
-        public static bool ConstantTracking = false;
+        public static bool AIAimAligner = true;
+        public static bool ConstantTracking = true;
         public static bool AimOnlyWhenBindingHeld = true;
 
-        public static bool AIAlwaysOn = false;
-        public static bool AIPredictions = false;
+        public static bool AIAlwaysOn = true;
+        public static bool AIPredictions = true;
 
         public static bool ShowFOV = false;
         public static bool TravellingFOV = false;
 
-        public static bool ShowDetectedPlayerWindow = false;
-        public static bool ShowCurrentDetectedPlayer = false;
+        public static bool ShowDetectedPlayerWindow = true;
+        public static bool ShowCurrentDetectedPlayer = true;
         public static bool ShowUnfilteredDetectedPlayer = false;
-        public static bool ShowPrediction = false;
+        public static bool ShowPrediction = true;
 
         public static bool ThirdPersonAim = false;
         public static bool Triggerbot = false;
